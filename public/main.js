1170c4235bd7b1d8f004b8070d1d89416e927c10
LuisAlpha <fuko2012@hotmail.com>
2019-01-10 17:36:42 -0600

    Agregar se agregaron dos funciones ya que no pude enviar un valor por promesa :3

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
-function DescargarAudios(event){
-	console.log('me paso de pendo :3');
+function DescargarTNS(event){
+	fetch('/TNS',{method:"POST"})
+	.then((response)=>{
+		if(response.ok){
+			Alert('Se Inicio el proceso de TNS');
+			return;
+		}
+		throw new Error('Request Failed.');
+	}).catch((error)=>{
+		console.error(error);
+	});
+}
+
+function DescargarIPSOS(event){
+	fetch('/IPSOS',{method:"POST" })
+	.then((response)=>{
+		if(response.ok){
+			Alert('Se inicio el proceso de IPSOS');
+			return;
+		}
+		throw new Error('Request Failed.');
+	}).catch((error)=>{
+		console.error(error);
+	});
 }
--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require('express');
+const exec = require('ssh-exec');
+
+
 
 const app = express();
-
 app.use(express.static('public'));
 
+var host_v = '172.30.27.9';
+
 app.listen(8080, () =>{
-console.log('listening on 8080');
+	console.log('listening on 8080');
 });
 
 app.get('/', (req, res)=>{
-res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/TNS', (req, res)=>{
+
+	exec('/home/Audios/Shells/./TNS_ren.sh',{
+		user: 'root',
+		host: '172.30.27.9',
+		password:'4lc0mAdm'
+	}).pipe(process.stdout, function(err, data){
+		if(err) {
+			console.log(v_host);
+			console.log(err);
+		}
+		console.log(data);
+	});
+
+	res.sendStatus(201);
+});
+
+app.post('/IPSOS', (req, res)=>{
+
+	exec('/home/Audios/Shells/./IPSOS_ren.sh',{
+		user: 'root',
+		host: '172.30.27.9',
+		password:'4lc0mAdm'
+	}).pipe(process.stdout, function(err, data){
+		if(err) {
+			console.log(v_host);
+			console.log(err);
+		}
+		console.log(data);
+	});
+
+	res.sendStatus(201);
 });
